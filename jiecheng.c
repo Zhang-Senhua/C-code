984f1b490936e20b48ff98538c3927c8a0f905a8
LUCKY-SEN2002 <75940443+LUCKY-SEN2002@users.noreply.github.com>
2021-01-19 23:12:07 +0800

    Add files via upload

--- jiecheng.c
+++ jiecheng.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int sum = 1;
-    
-    int SUM = 0;
 
+    int SUM = 0;
+    int n = 1;
+    int sum = 1;
     int num1;
     printf("请输入你要计算的阶乘数n:例如1!+2!+3!+...+n!\n");
     scanf("%d", &num1);
     for (int num2 = 1; num2 <= num1; num2++)
     {
-
-        for (int n = 1; n <= num2; n++)
-        {
-            sum = sum * n;
-        }
-
+        // int sum = 1;
+        // for (int n = 1; n <= num2; n++)
+        // {
+        //     sum = sum * n;
+        // }
+        sum = sum * num2;
 
         SUM = sum + SUM;
     }
